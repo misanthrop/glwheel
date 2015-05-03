39640f0fd7d6ae497a0798936772b81ce637cf14
Misanthrop <misanthrop@ukr.net>
2015-05-03 16:05:32 +0300

    Replaced spaces with tabs

--- signals.hpp
+++ signals.hpp
@@ -4,21 +4,21 @@
 
 namespace wheel
 {
-    using namespace std;
+	using namespace std;
 
 	template<class signature> struct sig;
 
 	template<class... args> struct sig<void(args...)>
-    {
+	{
 		using slot = function<void(args...)>;
 		using connection = typename list<slot>::iterator;
 		list<slot> slots;
-        connection begin() { return slots.begin(); }
-        connection end() { return slots.end(); }
+		connection begin() { return slots.begin(); }
+		connection end() { return slots.end(); }
 		connection operator+=(slot fn) { return slots.insert(slots.end(), fn); }
 		connection connect(slot fn, connection c) { return slots.insert(c, fn); }
-        connection operator-=(connection c) { return slots.erase(c); }
-        void clear() { slots.clear(); }
-        void operator()(args... a) { for(auto& s : slots) s(a...); }
-    };
+		connection operator-=(connection c) { return slots.erase(c); }
+		void clear() { slots.clear(); }
+		void operator()(args... a) { for(auto& s : slots) s(a...); }
+	};
 }
